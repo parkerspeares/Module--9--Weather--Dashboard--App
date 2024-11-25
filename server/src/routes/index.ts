9aabd9d4164327a205148e78e6ca6875059582ba
parkerspeares <parker.speares@outlook.com>
2024-11-24 22:44:25 -0800

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const router = Router();
 
 import apiRoutes from './api/index.js';
-import htmlRoutes from './htmlRoutes.js';
+import htmlRoutes from './htmlroutes.js';
 
 router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
